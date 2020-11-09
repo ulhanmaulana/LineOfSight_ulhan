465d922b0913abfd5257c46920c5066b4409e7a8
ulhanmaulana <ulhanmaulana@gmail.com>
2020-11-10 00:55:22 +0700

    upload pertama

---
+++ Assets/LineOfSightDemo/Scripts/AI.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AI : MonoBehaviour {
+
+	public GameObject Player;
+	public GameObject Enemy;
+	public float fov = 120f;
+	public float viewDistance = 10f;
+	private bool isAware = false;
+	private NavMeshAgent agent;
+	private Renderer renderer;
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+		renderer = GetComponent<Renderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (isAware) 
+		{
+			if ((Vector3.Angle (Vector3.forward, transform.InverseTransformPoint (Player.transform.position)) < fov / 2f) && Vector3.Distance (Player.transform.position, transform.position) < viewDistance) 
+			{
+				agent.SetDestination (Player.transform.position);
+				renderer.material.color = Color.black;
+			} else 
+			{
+				agent.SetDestination (Enemy.transform.position);
+				SearchForPlayer ();
+				renderer.material.color = Color.red;
+			}
+
+		} else
+		{
+			SearchForPlayer ();
+			renderer.material.color = Color.red;
+		}
+		
+	}
+	public void SearchForPlayer()
+	{
+		if (Vector3.Angle (Vector3.forward, transform.InverseTransformPoint (Player.transform.position)) < fov / 2f) 
+		{
+			if (Vector3.Distance (Player.transform.position, transform.position) < viewDistance) 
+			{	
+				RaycastHit hit;
+				if (Physics.Linecast (transform.position, Player.transform.position, out hit, -1)) 
+				{
+					if (hit.transform.CompareTag ("Player")) {
+						OnAware ();
+					}
+				}
+			}
+		} 
+			
+	}
+	public void OnAware()
+	{
+		isAware = true;
+	}
+}
---
+++ Assets/LineOfSightDemo/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+public class Player : MonoBehaviour {
+
+    CharacterController chara;
+
+	void Start () {
+        chara = GetComponent<CharacterController>();
+	}
+	
+	void Update () {
+        Move();
+        Look();
+	}
+
+    void Move()
+    {
+        float motionX = Input.GetAxis("Horizontal") * Time.deltaTime;
+        float motionZ = Input.GetAxis("Vertical") * Time.deltaTime; ;
+
+        Vector3 motion = new Vector3(motionX, 0f, motionZ);
+
+        chara.Move(motion);
+    }
+
+    
+	void Look()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+            Vector3 lookDir = new Vector3(hitInfo.point.x, transform.position.y, hitInfo.point.z);
+            Vector3 targetRotation = Quaternion.LookRotation(lookDir - transform.position).eulerAngles;
+            targetRotation = new Vector3(0, targetRotation.y, 0);
+
+            transform.rotation = Quaternion.Euler(targetRotation);
+        }
+    }
+}
